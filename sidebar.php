f2e6ea0e8524516b843fb189156723181e66f744
Conners Hua <ConnersHua@me.com>
2024-09-23 19:05:02 +0800

    feat: Add child-menu

--- sidebar.php
+++ sidebar.php
@@ -8,6 +8,12 @@
 		
 		<div class="sidebar-content">
 			
+			<?php if( is_page_template('page-templates/child-menu.php') ): ?>
+				<ul class="child-menu group">
+					<?php wp_list_pages('title_li=&sort_column=menu_order&depth=3'); ?>
+				</ul>
+			<?php endif; ?>
+
 			<?php dynamic_sidebar($sidebar); ?>
 			
 		</div><!--/.sidebar-content-->
